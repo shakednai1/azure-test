7d4713c17c0600dd6ea68908f043da7b6f1c06f3
Shakednai <Shakednai1@gmail.com>
2019-05-17 18:03:15 +0300

    test8

---
+++ application.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from azure.cognitiveservices.search.websearch import WebSearchAPI
+from azure.cognitiveservices.search.websearch.models import SafeSearch
+from msrest.authentication import CognitiveServicesCredentials
+
+app = Flask(__name__)
+
+subscription_key = "2e702b743ba744a6be5948828d355d6b"
+client = WebSearchAPI(CognitiveServicesCredentials(subscription_key))
+
+def search():
+    web_data = client.web.search(query="Beyonce")
+    for web_page in web_data.web_pages.value:
+        if web_page.url.startswith("https://www.instagram.com/"):
+            return web_page
+
+@app.route("/")
+def hello():
+    #search()
+    return "newnew"
+
