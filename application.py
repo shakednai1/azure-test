ea762ca175cf2fdddc150e97615e0c3ff6283062
Shakednai <Shakednai1@gmail.com>
2019-05-18 09:02:06 +0300

    connect to db

--- application.py
+++ application.py
@@ -1,13 +1,29 @@
 import requests
-from flask import Flask, render_template ,jsonify, request
+import pyodbc
+from flask import Flask, render_template, request
 from azure.cognitiveservices.search.websearch import WebSearchAPI
-from azure.cognitiveservices.search.websearch.models import SafeSearch
 from msrest.authentication import CognitiveServicesCredentials
 
 app = Flask(__name__)
 
+# bing search client connection
 subscription_key = "2e702b743ba744a6be5948828d355d6b"
 client = WebSearchAPI(CognitiveServicesCredentials(subscription_key))
+
+# sql database connection
+server = 'db-shaked-test.database.windows.net'
+database = 'db-shaked-test'
+username = 'shaked@db-shaked-test'
+password = 'Aa123456'
+driver= '{ODBC Driver 13 for SQL Server}'
+# import pdb; pdb.set_trace()
+cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='
+                                                                   ''+database+';UID='+username+';PWD='+ password)
+print('The cnection is workkkkkk' + cnxn)
+# cursor = cnxn.cursor()
+# cursor.execute("SELECT * from search_history")
+# all = cursor.fetchall()
+# import pdb; pdb.set_trace()
 
 
 def search_insta(val):
@@ -27,3 +43,6 @@ def search():
     to_search = request.args.get('value', 0)
     res = search_insta(to_search)
     return res
+
+# if __name__ == "__main__":
+# 	app.run()
