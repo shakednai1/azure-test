20bc5219ec2ff28c475d3753b2e07abe13f64847
Shakednai <Shakednai1@gmail.com>
2019-05-18 12:40:01 +0300

    database fixes

--- application.py
+++ application.py
@@ -11,19 +11,15 @@ subscription_key = "2e702b743ba744a6be5948828d355d6b"
 client = WebSearchAPI(CognitiveServicesCredentials(subscription_key))
 
 # sql database connection
-server = 'db-shaked-test.database.windows.net'
-database = 'db-shaked-test'
-username = 'shaked@db-shaked-test'
-password = 'Aa123456'
-driver= '{ODBC Driver 13 for SQL Server}'
-# import pdb; pdb.set_trace()
-cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='
-                                                                   ''+database+';UID='+username+';PWD='+ password)
-print('The cnection is workkkkkk' + cnxn)
-# cursor = cnxn.cursor()
-# cursor.execute("SELECT * from search_history")
-# all = cursor.fetchall()
-# import pdb; pdb.set_trace()
+# server = 'db-shaked-test.database.windows.net,1443'
+# database = 'db-shaked-test'
+# username = 'shaked@db-shaked-test'
+# password = 'Aa123456'
+# driver = '{SQL Server}'
+cnxn = pyodbc.connect("DRIVER={SQL Server};SERVER=db-shaked-test.database.windows.net,1433;DATABASE=db-shaked-test;"
+                      "UID=shaked@db-shaked-test;PWD=Aa123456")
+cursor = cnxn.cursor()
+
 
 
 def search_insta(val):
@@ -32,17 +28,26 @@ def search_insta(val):
         if web_page.url.startswith("https://www.instagram.com/"):
             return web_page.url
 
-
 @app.route("/")
 def hello():
     return render_template("index.html")
+
 
 @app.route("/search")
 def search():
     # TODO : find in data base - if not exist search from bing
     to_search = request.args.get('value', 0)
-    res = search_insta(to_search)
+    try:
+        res = get_result_from_db(to_search)
+    except IndexError:
+        res = search_insta(to_search)
     return res
+
+
+def get_result_from_db(to_search):
+    cursor.execute("SELECT * from dbo.search_history where search_name = '{}'".format(to_search))
+    all = cursor.fetchone()
+    return all[1]
 
 # if __name__ == "__main__":
 # 	app.run()
